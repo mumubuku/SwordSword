ab5aa4551eae803b7dbafd9a69dcf179be4d3667
tianyunzhao <tianyunzhao@soullink.com.cn>
2019-10-31 11:27:51 +0800

    Initial Commit

---
+++ SwordSword/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SwordSword
+//
+//  Created by ZHAO on 2019/10/31.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
